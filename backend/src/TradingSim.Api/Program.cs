2b976adbec84e4dd94c88502a6a175dbe1005b16
Toha Hossain <tohahossain16436@gmail.com>
2026-03-04 13:11:30 +0600

    Added new files

--- backend/src/TradingSim.Api/Program.cs
+++ backend/src/TradingSim.Api/Program.cs
@@ -1,41 +1,18 @@
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+var connectionString = builder.Configuration["Mongo:ConnectionString"];
+var databaseName = builder.Configuration["Mongo:Database"];
 
+var mongoClient = new MongoClient(connectionString);
+var database = mongoClient.GetDatabase(databaseName);
+
+// optional seed
+// await SeedData.SeedAsync(database);
+await SeedData.SeedAsync(database);
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.MapGet("/", () => "TradingSim API Running");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- backend/src/TradingSim.Application/obj/Debug/net10.0/TradingSim.Application.AssemblyInfo.cs
+++ backend/src/TradingSim.Application/obj/Debug/net10.0/TradingSim.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TradingSim.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d72af0e064e258b2f9fc3ca1bb594104c2f708b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac14a940b0d227a91782edac776889629e395aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("TradingSim.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TradingSim.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
